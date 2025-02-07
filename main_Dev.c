a35ef942a942a2ddf09e963f5d47da147efd33b9
ayush-2911 <ayush.savaliya@matrixcomsec.com>
2025-02-07 15:37:57 +0530

    main_dev.c file updated for conflict

--- main_Dev.c
+++ main_Dev.c
@@ -3,5 +3,8 @@
 int main()
 {
     printf("GitHub Training\n");
+    printf("hello, how are you???");
+    printf("hiiiii, hey hiiiiiii !!!!!!!!!!!!!");
+    printf("helllloooooooo!!!!!!!!!!!!!!!!");
     return 0;
 }
